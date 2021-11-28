5993b2132fb0b6c59f34a3976e4a0c7a2b2c4da4
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-11-28 16:25:50 +0300

    Add files via upload

---
+++ UQL.cpp
@@ -0,0 +1,40 @@
+﻿// UQL.cpp Main block of program UQL translator
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//we hold positions of symbols
+char SYMBOLS_POSITION[40] = {};
+
+string create = "CREATE TABLE";
+
+// Ready string for uql command
+string query_uql; 
+
+
+int main()
+{
+	
+	string query = "CREATE TABLE !table $name_string , $password_string;";
+	//check_command(query);
+	string command;
+	int point_char = query.find("!");
+	SYMBOLS_POSITION[0] = point_char;
+	//check for CREATE TABLE
+	for (int i = 0; i <= point_char; i++) {
+		command[i] = query[i];	
+	}
+	if (command.find(create)){
+		cout << command << endl;
+	}
+	else {
+		cout << command;
+	}
+	
+	
+	return 0;
+}
+
